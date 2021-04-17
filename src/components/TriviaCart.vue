467296cdba2e0de2a6dd4e176aa80ebffac23e11
BwieBasti86 <sebastian.jacob@gmx.ch>
2021-04-18 01:11:17 +0200

    Added TriviaCart to components

---
+++ src/components/TriviaCart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cart">
+    {{ triviaItem.question }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { TriviaItem } from "../interfaces/TriviaItem";
+
+@Component
+export default class TriviaCart extends Vue {
+  @Prop() private triviaItem!: TriviaItem;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cart {
+  display: block;
+}
+</style>
